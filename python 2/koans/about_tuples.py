77b9021952522b2e3408fba73065c4cfb5999396
Floris Lambrechts <florisla@gmail.com>
2012-06-24 14:06:13 +0200

    Complete next Koan

--- python 2/koans/about_tuples.py
+++ python 2/koans/about_tuples.py
@@ -44,11 +44,11 @@ class AboutTuples(Koan):
         self.assertEqual(tuple(['Hello comma!']), ("Hello comma!", ))
         
     def test_tuple_constructor_can_be_surprising(self):
-        self.assertEqual(__, tuple("Surprise!"))
+        self.assertEqual(('S','u','r','p','r','i','s','e','!'), tuple("Surprise!"))
 
     def test_creating_empty_tuples(self):
         self.assertEqual(__, ())
-        self.assertEqual(__, tuple())  # Sometimes less confusing
+        self.assertEqual(__, tuple())  # Sometimes l'ess confusing
         
     def test_tuples_can_be_embedded(self):
         lat = (37, 14, 6, 'N')
