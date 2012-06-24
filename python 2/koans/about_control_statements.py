a8fbb928b9d66619f6be3852ce8819ef6d748c15
Floris Lambrechts <florisla@gmail.com>
2012-06-24 14:36:51 +0200

    Complete next Koan

--- python 2/koans/about_control_statements.py
+++ python 2/koans/about_control_statements.py
@@ -64,7 +64,7 @@ class AboutControlStatements(Koan):
             result.append("Contestant: '" + knight + \
             "'   Answer: '" + answer + "'")
 
-        text = __
+        text = "Contestant: 'Robin'   Answer: 'Blue! I mean Green!'"
 
         self.assertMatch(text, result[2])
 
