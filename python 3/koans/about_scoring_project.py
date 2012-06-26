b1edecad95d93e90bfff5797d2af13f1f12caa5b
Floris Lambrechts <floris.lambrechts@onsemi.com>
2012-06-26 10:39:39 +0200

    Complete next Koan

--- python 3/koans/about_scoring_project.py
+++ python 3/koans/about_scoring_project.py
@@ -10,7 +10,7 @@ from runner.koan import *
 # A greed roll is scored as follows:
 #
 # * A set of three ones is 1000 points
-#   
+#
 # * A set of three numbers (other than ones) is worth 100 times the
 #   number. (e.g. three fives is 500 points).
 #
@@ -34,47 +34,56 @@ from runner.koan import *
 
 def score(dice):
     score = 0
-    fives = len([d for d in dice if d==5])
-    ones = len([d for d in dice if d==1])
-    if fives < 3:
-    	score += fives * 50
-    if ones == 3:
-    	score += 1000
-    elif ones < 3:
-    	score += ones * 100
+    counts = [-1, 0, 0, 0, 0, 0, 0]
+
+    for die in dice:
+        counts[die] += 1
+
+    if counts[5] > 0:
+        score += counts[5] * 50
+        counts[5] = 0
+
+    while counts[1] >= 3:
+        score += 1000
+        counts[1] -= 3
+
+    if counts[1] > 0:
+        score += counts[1] * 100
+        counts[1] = 0
+
     return score
 
 class AboutScoringProject(Koan):
     def test_score_of_an_empty_list_is_zero(self):
         self.assertEqual(0, score([]))
-    
+
     def test_score_of_a_single_roll_of_5_is_50(self):
         self.assertEqual(50, score([5]))
-    
+
     def test_score_of_a_single_roll_of_1_is_100(self):
         self.assertEqual(100, score([1]))
-    
+
     def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores(self):
         self.assertEqual(300, score([1,5,5,1]))
-    
+
     def test_score_of_single_2s_3s_4s_and_6s_are_zero(self):
         self.assertEqual(0, score([2,3,4,6]))
-    
+
     def test_score_of_a_triple_1_is_1000(self):
         self.assertEqual(1000, score([1,1,1]))
-    
+
     def test_score_of_other_triples_is_100x(self):
         self.assertEqual(200, score([2,2,2]))
         self.assertEqual(300, score([3,3,3]))
         self.assertEqual(400, score([4,4,4]))
         self.assertEqual(500, score([5,5,5]))
         self.assertEqual(600, score([6,6,6]))
-    
+
     def test_score_of_mixed_is_sum(self):
         self.assertEqual(250, score([2,5,2,2,3]))
         self.assertEqual(550, score([5,5,5,5]))
         self.assertEqual(1150, score([1,1,1,5,1]))
-        
+
     def test_ones_not_left_out(self):
         self.assertEqual(300, score([1,2,2,2]))
         self.assertEqual(350, score([1,5,2,2,2]))
